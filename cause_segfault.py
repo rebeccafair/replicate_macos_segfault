c5ef3b177aa0498018149468021e3f2bd2e7cfba
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-09-21 14:56:00 +0100

    Diagonalise last array only

--- cause_segfault.py
+++ cause_segfault.py
@@ -18,8 +18,6 @@ np.random.seed(0)
 dmats = np.random.rand(*shape) + 1j*np.random.rand(*shape)
 evals = np.zeros((dmats.shape[0], dmats.shape[1]), dtype=np.float64)
 
-segfault.diagonalise(evals, dmats[:-1], 1)
-print('Diagonalised first part', flush=True)
 segfault.diagonalise(evals, dmats[-1:], 1)
 
 print('Done!')
